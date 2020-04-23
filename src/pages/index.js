2e918d4369944fc79c7e465767c608fe5e16d73c
Kennan Smith <kennan@watermarkadvertising.net>
2020-04-23 16:58:00 -0600

    more cleaning out to get to the basic starting point

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,7 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import styled from 'styled-components'
 import { Container, Row, Col } from 'react-bootstrap'
-import get from 'lodash/get'
-import Helmet from 'react-helmet'
+// import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 
 const ContentCont = styled(Container)`
@@ -12,14 +11,11 @@ const ContentCont = styled(Container)`
 
 class RootIndex extends React.Component {
   render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const posts = get(this, 'props.data.allContentfulBlogPost.edges')
-    const [author] = get(this, 'props.data.allContentfulPerson.edges')
 
     return (
       <Layout location={this.props.location}>
         <div style={{ background: 'white' }}>
-          <Helmet title={siteTitle} />
+          {/* <Helmet title={siteTitle} /> */}
           <ContentCont className="wrapper">
             page content
           </ContentCont>
@@ -33,48 +29,9 @@ export default RootIndex
 
 export const pageQuery = graphql`
   query HomeQuery {
-    allContentfulBlogPost(sort: { fields: [publishDate], order: DESC }) {
-      edges {
-        node {
-          title
-          slug
-          publishDate(formatString: "MMMM Do, YYYY")
-          tags
-          heroImage {
-            fluid(maxWidth: 350, maxHeight: 196, resizingBehavior: SCALE) {
-              ...GatsbyContentfulFluid_tracedSVG
-            }
-          }
-          description {
-            childMarkdownRemark {
-              html
-            }
-          }
-        }
-      }
-    }
-    allContentfulPerson(
-      filter: { contentful_id: { eq: "15jwOBqpxqSAOy2eOO4S0m" } }
-    ) {
-      edges {
-        node {
-          name
-          shortBio {
-            shortBio
-          }
-          title
-          heroImage: image {
-            fluid(
-              maxWidth: 1180
-              maxHeight: 480
-              resizingBehavior: PAD
-              background: "rgb:000000"
-            ) {
-              ...GatsbyContentfulFluid_tracedSVG
-            }
-          }
-        }
-      }
+    contentfulPage(title: {eq: "Home"}) {
+      title
+      slug
     }
   }
 `
