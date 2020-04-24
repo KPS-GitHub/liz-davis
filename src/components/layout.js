ebce84832a4b7d5c547cbb7e25dd3a284d9dd117
Kennan Smith <kennan@watermarkadvertising.net>
2020-04-23 18:24:45 -0600

    header is ready to roll

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,23 +1,49 @@
 import React from 'react'
+import { Link, useStaticQuery, graphql } from 'gatsby'
 import styled from 'styled-components'
 import { Container } from 'react-bootstrap'
 import Navigation from './navigation'
+import Img from 'gatsby-image'
+
 
 const WrapCont = styled(Container)`
   margin: 0;
+  .logo-wrap {
+    width: 45%;
+    a {
+      display: block;
+      padding: 10px 0;
+      .gatsby-image-wrapper {
+        width: 100%;
+      }
+    }
+  }
 `
 
-class Template extends React.Component {
-  render() {
-    const { children } = this.props
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      logo: contentfulAsset(title: {eq: "Official Logo"}) {
+        title
+        description
+        fluid {
+          ...GatsbyContentfulFluid
+        }
+      }
+    }
+  `)
+  console.log(data)
+  const { logo } = data
 
-    return (
-      <WrapCont>
-        <Navigation />
-        {children}
-      </WrapCont>
-    )
-  }
+  return (
+    <WrapCont>
+      <div className="logo-wrap">
+        <Link to={`/`}><Img fluid={logo.fluid} alt={logo.description} /></Link> 
+      </div>
+      <Navigation />
+      {children}
+    </WrapCont>
+  )
 }
 
-export default Template
+export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import styled from 'styled-components'
 import { Container, Row, Col } from 'react-bootstrap'
-// import Helmet from 'react-helmet'
+import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 
 const ContentCont = styled(Container)`
@@ -13,9 +13,24 @@ class RootIndex extends React.Component {
   render() {
 
     return (
-      <Layout location={this.props.location}>
+      <Layout >
         <div style={{ background: 'white' }}>
-          {/* <Helmet title={siteTitle} /> */}
+          <Helmet>
+            <title>Elizabeth Davis - EMDR and Art Therapy</title>
+            <style type="text/css">{`
+              body {
+                display: contents;
+              }
+              #gatsby-focus-wrapper {
+                width: 100%;
+                height: 100%;
+              }
+              #___gatsby {
+                width: 100%;
+                height: 100%;
+              }
+            `}</style>
+          </Helmet>
           <ContentCont className="wrapper">
             page content
           </ContentCont>
