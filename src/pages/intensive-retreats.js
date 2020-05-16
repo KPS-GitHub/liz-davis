361c941c43cb6192b2ae17de4387933d63354058
KennanSmith <kennan.smith1@gmail.com>
2020-05-16 12:19:23 -0600

    added basic content to retreats and prof serv pages

---
+++ src/pages/intensive-retreats.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import styled from 'styled-components'
+import Layout from '../components/Layout/layout'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+
+const Wrap = styled.div`
+    padding: 3vw 15vw 15vw 15vw;
+    h1 {
+        text-align: center;
+        margin-bottom: 4rem;
+    }
+    a {
+        color: blue;
+    }
+`
+
+const IntensiveRetreats = () => {
+    const pageContent = useStaticQuery(graphql`
+        query {
+            contentfulPage(title: {eq: "Intensive Retreats"}) {
+                pageContent {
+                    json
+                }
+            }
+        }
+    `).contentfulPage.pageContent.json;
+    return(
+        <Layout>
+            <Wrap>
+                {documentToReactComponents(pageContent)}
+            </Wrap>
+        </Layout>    
+    )
+}
+
+export default IntensiveRetreats;
